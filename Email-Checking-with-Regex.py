fc7a06b58892a95c2e881bd16edc45655bb39daf
GuanRuLai <128710130+GuanRuLai@users.noreply.github.com>
2023-07-18 16:58:05 +0800

    Add files via upload

---
+++ Email-Checking-with-Regex.py
@@ -0,0 +1,17 @@
+import re
+# ????@??.??
+regex = r"\b[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+def check_email(email):
+    if re.fullmatch(regex, email):
+        print(f"{email} is a valid email.")
+    else:
+        print(f"{email} is invalid.")
+email1 = "shfidoid@gmail.com"
+email2 = "myown.site@our-earth.org"
+email3 = "hahah.com"
+check_email(email1)
+check_email(email2)
+check_email(email3)
+#%%
+
+#%%
